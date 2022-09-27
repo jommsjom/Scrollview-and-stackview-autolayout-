cc5d915c83685f326a010c977265989a7f42ef8b
jommsjom <81458112+jommsjom@users.noreply.github.com>
2022-09-27 22:29:48 +0530

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  task1_normal
+//
+//  Created by Jomms on 25/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+       
+    }
+
+
+}
+
